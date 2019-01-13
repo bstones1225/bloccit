650001df268484829923b18869bd43e5204f54e6
Brian Stones <bstones1225@hotmail.com>
2019-01-13 00:47:06 -0600

    Checkpoint Voting finished

--- src/db/models/comment.js
+++ src/db/models/comment.js
@@ -12,6 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     userId: {
       type: DataTypes.INTEGER,
       allowNull: false
+    }
     }, {});
   Comment.associate = function(models) {
     Comment.belongsTo(models.Post, {
