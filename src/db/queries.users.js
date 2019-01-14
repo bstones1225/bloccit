57d587bc4480412dcf9fee13e1720c5d6f7ae9d4
Brian Stones <bstones1225@hotmail.com>
2019-01-14 04:12:57 -0600

    Checkpoint Profiles done

--- src/db/queries.users.js
+++ src/db/queries.users.js
@@ -1,5 +1,7 @@
 // #1
 const User = require("./models").User;
+const Post = require("./models").Post;
+const Comment = require("./models").Comment;
 const bcrypt = require("bcryptjs");
 
 module.exports = {
@@ -21,6 +23,35 @@ module.exports = {
     .catch((err) => {
       callback(err);
     })
+  },
+  getUser(id, callback){
+ // #1
+    let result = {};
+    User.findById(id)
+    .then((user) => {
+ // #2
+      if(!user) {
+        callback(404);
+      } else {
+ // #3
+        result["user"] = user;
+ // #4
+        Post.scope({method: ["lastFiveFor", id]}).all()
+        .then((posts) => {
+ // #5
+          result["posts"] = posts;
+ // #6
+          Comment.scope({method: ["lastFiveFor", id]}).all()
+          .then((comments) => {
+ // #7
+            result["comments"] = comments;
+            callback(null, result);
+          })
+          .catch((err) => {
+            callback(err);
+          })
+        })
+      }
+    })
   }
-
 }
